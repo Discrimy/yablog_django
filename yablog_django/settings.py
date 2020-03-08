f7f32410ee1a6bd9e4bc19bd6e1e236507869192
Alexander Bespalov <discrimy.off@gmail.com>
2020-03-08 18:56:30 +0300

    Fix static folder

--- yablog_django/settings.py
+++ yablog_django/settings.py
@@ -17,6 +17,9 @@ import re
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
